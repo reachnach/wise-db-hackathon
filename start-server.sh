0eef9ed51614580760f5e1eb21405eaa35655fc6
Nachiappan Narayanan <reachnach@gmail.com>
2019-06-04 16:26:05 -0400

    Fixed the startup script

--- start-server.sh
+++ start-server.sh
@@ -1,1 +1,1 @@
-java -Xdebug -Xrunjdwp:server=y, transport=dt_socket, address=8000, suspend=n -jar target/wise-db-hackathon-server-0.1.0.jar
+java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 -jar target/wise-db-hackathon-server-0.1.0.jar
