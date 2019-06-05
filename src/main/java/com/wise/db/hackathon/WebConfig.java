c9c0febfbd94aee7c65d30b4585865843153bd4c
Nachiappan Narayanan <reachnach@gmail.com>
2019-06-05 17:03:44 -0400

    Adding WebConfig for CORS support

---
+++ src/main/java/com/wise/db/hackathon/WebConfig.java
@@ -0,0 +1,15 @@
+package com.wise.db.hackathon;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurationSupport {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
